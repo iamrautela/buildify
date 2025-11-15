13d769a5f71b2bf66f6e439551b60eec81cdd97f
iamrautela <abhisheksinghrautela036@gmail.com>
2025-11-15 19:57:57 +0530

    added prebuilt ui using tailwind css and fix routing and created the landing page of the page the webapp

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
     <div>
         <h1>Layout Page</h1>
+        <div>
+          <Outlet>
+            
+          </Outlet>
+        </div>
     </div>
   )
 }
