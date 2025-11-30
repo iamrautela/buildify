ebfabc61c2a9c9bca853c7b085089a48a7dfcf78
iamrautela <abhisheksinghrautela036@gmail.com>
2025-11-30 21:09:14 +0530

    added book user icon

--- client/src/components/home/Testimonial.jsx
+++ client/src/components/home/Testimonial.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Title from './Title'
+import { BookUserIcon } from 'lucide-react';
 
 const Testimonial = () => {
     return (
         <div id='testimonials' className='flex flex-col items-center my-10 scroll-mt-12'>
             <div className="flex items-center gap-2 text-sm text-green-800 bg-green-400/10 rounded-full px-6 py-1.5">
-                <Zap width={14} />
+                <BookUserIcon width={14} />
 
                 <span>Simple Process</span>
 
