363546f556756668be9aba4bbf9fe003d9f36ee8
iamrautela <abhisheksinghrautela036@gmail.com>
2025-11-30 20:58:49 +0530

    Added testimonalis

---
+++ client/src/components/home/Testimonial.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Title from './Title'
+
+const Testimonial = () => {
+    return (
+        <div id='testimonials' className='flex flex-col items-center my-10 scroll-mt-12'>
+            <div className="flex items-center gap-2 text-sm text-green-800 bg-green-400/10 rounded-full px-6 py-1.5">
+                <Zap width={14} />
+
+                <span>Simple Process</span>
+
+            </div>
+            <Title title='Build Your resume' description='Our streamlined process helps you create a professional resume in minutes with intelligent AI-powered tools and features.' />
+
+        </div>
+    )
+}
+
+export default Testimonial
