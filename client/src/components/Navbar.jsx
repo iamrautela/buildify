8da74f8a1ad2f830a564ab28ab5b7bcf8bfd3605
iamrautela <abhisheksinghrautela036@gmail.com>
2025-12-12 21:47:32 +0530

    added navbar basic structure

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link, Navigate } from 'react-router-dom'
+
+const Navbar = () => {
+    const user = {name: 'John Doe'} 
+    const navigate = useNavigate();
+
+    const logoutUser = () => {
+        navigate('/');
+  return (
+    <div classNmae='shadow bg-white'>
+        <nav className='flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5 text-slate-800 transition-all'>
+        <Link to='/'>
+        <img src="/logo.svg" alt="logo" classNane="h-11 w-auto" />
+        
+        </Link>
+        <div>
+            <p> Hi, {user?.name}</p>
+            <button onClick={logoutUser} className='bg-white hover:bg-slate-50 border-gray-300 px-7 py-1.5 rounded-full active:scale-95 trabsition-all'>
+                Logout
+            </button>
+        </div>
+        </nav>
+
+    </div>
+  )
+}
+
+export default Navbar
