b154e7ed34b97817da419c2e895cc902d8ddf5c9
iamrautela <abhisheksinghrautela036@gmail.com>
2025-12-12 21:32:10 +0530

    added green color in login sign up page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -69,19 +69,19 @@ const Login = () => {
 
                 </div>
 
-                <div className="mt-4 text-left text-indigo-500">
+                <div className="mt-4 text-left text-green-500">
 
                     <button className="text-sm" type="reset">Forget password?</button>
 
                 </div>
 
-                <button type="submit" className="mt-2 w-full h-11 rounded-full text-white bg-indigo-500 hover:opacity-90 transition-opacity">
+                <button type="submit" className="mt-2 w-full h-11 rounded-full text-white bg-green-500 hover:opacity-90 transition-opacity">
 
                     {state === "login" ? "Login" : "Sign up"}
 
                 </button>
 
-                <p onClick={() => setState(prev => prev === "login" ? "register" : "login")} className="text-gray-500 text-sm mt-3 mb-11">{state === "login" ? "Don't have an account?" : "Already have an account?"} <a href="#" className="text-indigo-500 hover:underline">click here</a></p>
+                <p onClick={() => setState(prev => prev === "login" ? "register" : "login")} className="text-gray-500 text-sm mt-3 mb-11">{state === "login" ? "Don't have an account?" : "Already have an account?"} <a href="#" className="text-green-500 hover:underline">click here</a></p>
 
             </form>
     </div>
