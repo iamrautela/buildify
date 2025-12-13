0a356c400949f4698c25a573ebe64cd277576113
iamrautela <abhisheksinghrautela036@gmail.com>
2025-12-13 21:58:35 +0530

    added dashboard seaction

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, Navigate } from 'react-router-dom'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
     const user = {name: 'John Doe'} 
@@ -10,15 +10,15 @@ const Navbar = () => {
     }
 
   return (
-    <div classNmae='shadow bg-white'>
+    <div className='shadow bg-white'>
         <nav className='flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5 text-slate-800 transition-all'>
         <Link to='/'>
-        <img src="/logo.svg" alt="logo" classNane="h-11 w-auto" />
+        <img src="/logo.svg" alt="logo" className="h-11 w-auto" />
         
         </Link>
         <div className='flex items-center gap-4 text-sm'>
             <p className='max-sm:hidden'> Hi, {user?.name}</p>
-            <button onClick={logoutUser} className='bg-white hover:bg-slate-50 border-gray-300 px-7 py-1.5 rounded-full active:scale-95 trabsition-all'>
+            <button onClick={logoutUser} className='bg-white hover:bg-slate-50 border-gray-300 px-7 py-1.5 rounded-full active:scale-95 transition-all'>
                 Logout
             </button>
         </div>
