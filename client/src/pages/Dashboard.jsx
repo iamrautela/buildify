5389c9b431e56662986c08ff3377ba441dd0432d
iamrautela <abhisheksinghrautela036@gmail.com>
2025-12-15 21:11:45 +0530

    added upload resume section

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { PlusIcon } from 'lucide-react'
+import { PlusIcon, UploadCloudIcon } from 'lucide-react'
 import React from 'react'
 
 const Dashboard = () => {
@@ -11,6 +11,10 @@ const Dashboard = () => {
             <button className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer'>
               <PlusIcon className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-indigo-300 to-indigo-500 text-white rounded-full'/>
               <p className='text-sm group-hover:text-indigo-600 transition-all duration-300'>Create Resume</p>
+            </button>
+             <button className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-purple-500 hover:shadow-lg transition-all duration-300 cursor-pointer'>
+              <UploadCloudIcon className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-purple-300 to-purple-500 text-white rounded-full'/>
+              <p className='text-sm group-hover:text-purple-600 transition-all duration-300'>Upload Existing</p>
             </button>
 
           </div>
