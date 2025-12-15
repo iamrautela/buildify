2235a3cef4f4969c6fcfb47abe5b4a3d733fe260
iamrautela <abhisheksinghrautela036@gmail.com>
2025-12-15 21:42:49 +0530

    Add Dashboard component with side-by-side

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,13 +1,28 @@
 import { PlusIcon, UploadCloudIcon } from 'lucide-react'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {dummyResumeData} from '../assets/assets.js'
 
 const Dashboard = () => {
+
+  const [allResumes, setAllResumes] = useState([])
+
+  const loadAllResumes = async () => {
+    setAllResumes(dummyResumeData)
+  }
+
+  useEffect(()=>{
+    const fetchResumes = async () => {
+      await loadAllResumes()
+    }
+    fetchResumes()
+
+  }, [])
   return (
     <div>
         <div className='max-w-7xl mx-auto px-4 py-8'>
-          <p className='text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to slate-700 bg-clip-text text-transparent sm:hidden'>Welcome, Joe Doe</p>
+          <p className='text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden'>Welcome, Joe Doe</p>
 
-          <div className='flex-gap-4'>
+          <div className='flex gap-4'>
             <button className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer'>
               <PlusIcon className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-indigo-300 to-indigo-500 text-white rounded-full'/>
               <p className='text-sm group-hover:text-indigo-600 transition-all duration-300'>Create Resume</p>
@@ -17,6 +32,13 @@ const Dashboard = () => {
               <p className='text-sm group-hover:text-purple-600 transition-all duration-300'>Upload Existing</p>
             </button>
 
+          </div>
+
+          <hr className='border-slate-300 my-6 sm:w-[305px]'/>
+          <div>
+            {allResumes.length > 0 && (
+              <p className='text-slate-500'>Your Resumes: {allResumes.length}</p>
+            )}
           </div>
 
         </div>
