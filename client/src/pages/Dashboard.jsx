24208439eb797b94977180e43099e5a0466e8f16
iamrautela <abhisheksinghrautela036@gmail.com>
2025-12-17 21:02:54 +0530

    rebuildind dasboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,26 +1,25 @@
-import {PlusIcon, UploadCloudIcon} from 'lucide-react';
-import React from 'react';
-import {useEffect, useState} from 'react'
-import {dummyResumeData} from '../assets/assets'
+import React from 'react'
+import { PlusIcon } from 'lucide-react'
 
 const Dashboard = () => {
-  const[allResumes, setAllResumes] = useState([])
-
-  const loadAllResumes = async () => {
-    setAllResumes(dummyResumeData)
-  }
-
-  useEffect(() => {
-    loadAllResumes()
-  }, [])
-
   return (
     <div>
       <div className='max-w-7xl mx-auto px-4 py-8'>
+        <p className='text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden'>
+          Welcome Joe Doe
+        </p>
+        <div className='flex gap-4'>
+          <button>
+            <PlusIcon className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-indigo-300 to-indigo-500 text-while rounded-full'/>
+            <p className='text-sm group-hover:text-indigo-600 transition-all duration-300'>Create Resume</p>
+          </button>
 
-        <p className='text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600
-        to-slate-700 bg-clip-text text-transparent sm:hidden'>Welcome, John Doe</p>
+        </div>
 
       </div>
-      <div></div>
+
     </div>
+  )
+}
+
+export default Dashboard
