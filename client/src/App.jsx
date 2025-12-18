c5160a05d26a385a61bed6b1d9bc894490d289be
iamrautela <abhisheksinghrautela036@gmail.com>
2025-12-18 20:43:31 +0530

    Fix route path for Layout component in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
         <Routes>
         <Route path='/' element={<Home />}/>
 
-        <Route path='app' element={<Layout />}>
+        <Route path='/app' element={<Layout />}>
           <Route index element={<Dashboard />}/>
           <Route path='builder/:resumeId' element={<ResumeBuilder />}/>
         </Route>
